c3bfbf90164ec6af37c8fe44017e7080bff2cd83
victor-h-castro <victor_h_castro@estudante.sesisenai.org.br>
2022-01-28 16:38:00 -0300

    feature : default layout before routing and material theme for extend styledcomponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable import/no-unresolved */
-import { Testing } from 'component/Testing';
 import { SettingsContext } from 'context/SettingContext';
+import DefaultLayout from 'layout/DefaultLayout';
 import React, { useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { MyGlobalStyle } from 'theme/GlobalStyles';
 import { dark, light } from 'theme/my-theme';
+import { ThemeProvider as ThemeProviderMUI, createTheme, useTheme } from '@mui/material/styles';
 
 function App() {
   const theme = useContext(SettingsContext);
-  console.log(theme.themeMode);
+  const defaultThemeMUI = useTheme();
+  const themeMUI = createTheme(defaultThemeMUI);
+
   return (
+    <ThemeProviderMUI theme={themeMUI}>
+      <ThemeProvider theme={theme.themeMode === 'light' ? light : dark}>
 
-    <ThemeProvider theme={theme.themeMode === 'light' ? light : dark}>
-
-      <MyGlobalStyle />
-      <Testing />
-      <input />
-    </ThemeProvider>
+        <MyGlobalStyle />
+        <DefaultLayout />
+      </ThemeProvider>
+    </ThemeProviderMUI>
   );
 }
 

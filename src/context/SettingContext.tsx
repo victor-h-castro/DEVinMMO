bbccf3bf56047b77a7ade7e3ca6064ae31e8ea16
victor-h-castro <93954551+victor-h-castro@users.noreply.github.com>
2022-02-01 21:47:36 -0300

    Revert "Feature : Header, SearchBar, AnimatedComponet, GameCard components"

--- src/context/SettingContext.tsx
+++ src/context/SettingContext.tsx
@@ -1,13 +1,10 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable import/no-unresolved */
-import { ReactNode, createContext, useState } from 'react';
+import { ReactNode, createContext } from 'react';
 import { ContextProps } from 'type/theme';
 
 const initialState: ContextProps = {
   themeMode: 'light',
-  state: '',
-  setState: (statee: any) => { },
 };
 
 const SettingsContext = createContext(initialState);
@@ -17,10 +14,11 @@ type SettingsProviderProps = {
 };
 
 function SettingsProvider({ children }: SettingsProviderProps) {
-  const [state, setState] = useState('');
   return (
     <SettingsContext.Provider
-      value={{ ...initialState, state, setState }}
+      value={{
+        ...initialState,
+      }}
     >
       {children}
     </SettingsContext.Provider>
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,14 +1,10 @@
-/* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
 /* eslint-disable import/prefer-default-export */
 
-import { Container, Grid } from '@mui/material';
-import ImageCard from 'component/ImageCard';
-import { SettingsContext } from 'context/SettingContext';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useMmoService } from 'service/useMmoService';
 import { GameProps } from 'type/game';
 import { GameListProps } from 'type/gameList';
@@ -18,16 +14,13 @@ export const Home = () => {
   const { fetchGameListData, fetchNewsListData, fetchGameData } = useMmoService();
   const [loading, setLoading] = useState<boolean>(false);
   const [gameList, setGameList] = useState<GameListProps[]>();
-  const [gameListRender, setGameListRender] = useState<GameListProps[]>();
   const [newsList, setNewsList] = useState<NewsListProps[]>();
   const [game, setGame] = useState<GameProps>();
-  const { state } = useContext(SettingsContext);
 
   const fetchGameList = async () => {
     setLoading(true);
     const gameListData :GameListProps[] = await fetchGameListData();
     setGameList(() => gameListData);
-    setGameListRender(() => gameListData);
     setLoading(() => true);
   };
 
@@ -49,27 +42,8 @@ export const Home = () => {
       await fetchGameData('2');
     })();
   }, []);
-  useEffect(() => {
-    setGameListRender((previous) => {
-      return gameList?.filter((element) => element.title.toLowerCase().includes(state));
-    });
-  }, [state]);
   return (
-    <Grid direction="row" spacing={5} container px={2} sx={{ marginTop: 1 }}>
-      <Grid container item spacing={5} xs={12}>
-        {gameListRender?.map(({
-          id, title, thumbnail, short_description,
-        }) => (
-          <Grid item xs={12} md={6} lg={4}>
-            <ImageCard
-              id={id}
-              title={title}
-              thumbnail={thumbnail}
-              description={short_description}
-            />
-          </Grid>
-        )) }
-      </Grid>
-    </Grid>
+
+    <h1>TESTE</h1>
   );
 };

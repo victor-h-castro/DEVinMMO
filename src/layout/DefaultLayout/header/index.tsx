ffb0c3f82d3622c7d8a814e36a7fed0a7a1788cd
victor-h-castro <victor_h_castro@estudante.sesisenai.org.br>
2022-02-06 16:55:58 -0300

    Fix : hide searchbar in game page

--- src/layout/DefaultLayout/header/index.tsx
+++ src/layout/DefaultLayout/header/index.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import { AppBar, Grid, Toolbar } from '@mui/material';
 import { TabHome } from 'component/TabHome';
 import ThemeSwitch from 'component/ThemeSwitch';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Searchbar from './SearchBar';
 
@@ -15,12 +17,14 @@ min-height: 96px;
 `;
 
 export default function Header() {
+  const { pathname } = useLocation();
+
   return (
     <AppBarStyled>
       <ToolBarStyled>
         <Grid container justifyContent="flex-start" direction="row">
           <Grid item xs={2}>
-            <Searchbar />
+            { !(pathname.includes('/game')) && <Searchbar />}
           </Grid>
           <Grid item container alignItems="center" justifyContent="center" xs={8}>
 

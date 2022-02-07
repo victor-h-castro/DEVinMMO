742377f44505d245040392ebe95f841d1767b7cf
victor-h-castro <victor_h_castro@estudante.sesisenai.org.br>
2022-02-06 21:47:47 -0300

    Feature : function to shuffle array at home page

--- src/page/News.tsx
+++ src/page/News.tsx
@@ -9,15 +9,12 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable import/prefer-default-export */
 
-import { Container, Grid } from '@mui/material';
-import Carousel from 'component/Carousel';
-import ImageCard from 'component/ImageCard';
+import { Grid } from '@mui/material';
 import { Loading } from 'component/Loading';
 import NewsCard from 'component/NewsCard';
 import { SettingsContext } from 'context/SettingContext';
 import { useContext, useEffect, useState } from 'react';
 import { useMmoService } from 'service/useMmoService';
-import { GameProps } from 'type/game';
 import { GameListProps } from 'type/gameList';
 import { NewsListProps } from 'type/newsList';
 
@@ -27,18 +24,11 @@ export const News = () => {
   const [gameListRender, setGameListRender] = useState<GameListProps[]>();
   const [newsList, setNewsList] = useState<NewsListProps[]>();
   const { state, setState } = useContext(SettingsContext);
-  function shuffleArr(array: any[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const rand = Math.floor(Math.random() * (i + 1));
-      [array[i], array[rand]] = [array[rand], array[i]];
-    }
-  }
 
   const fetchNewsList = async () => {
     try {
       setLoading(() => true);
       const newsListData :NewsListProps[] = await fetchNewsListData();
-      shuffleArr(newsListData);
       setNewsList(() => newsListData);
       setLoading(() => false);
     } catch {
---
+++ src/util/shuffleArray.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/prefer-default-export */
+export function shuffleArr(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    [array[i], array[rand]] = [array[rand], array[i]];
+  }
+}
